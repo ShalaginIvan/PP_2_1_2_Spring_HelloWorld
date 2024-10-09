89f4a0bc6b8620878ac328a89827de1fb2cd5346
Ivan Shalagin <ivan-shalagin@list.ru>
2024-10-09 18:52:46 +0500

    release

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,18 @@ public class App {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+                (HelloWorld) applicationContext.getBean("helloworld", HelloWorld.class);
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("Переменные класса HelloWorld ссылаются на один и тот же объект? " + (bean == bean1));
+        System.out.println("Переменные класса Cat ссылаются на один и тот же объект? " + (cat1 == cat2));
+
+        ((AnnotationConfigApplicationContext)applicationContext).close();
     }
 }
